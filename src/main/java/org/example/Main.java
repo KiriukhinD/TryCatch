b1d91e515411e2389a30e50c19283a63e21ca50a
KiriukhinD <dm.arz2018@yandex.ru>
2022-11-30 10:54:18 +0300

    TryCatch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,26 @@
 package org.example;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        Main main = new Main();
+        try {
+            main.getPokaz();
+
+        } catch (IOException e) {
+            System.out.println("error error error");
+        }
 
 
+    }
 
+    void getPokaz() throws IOException {
 
+        int a = 7;
+        if (a == 5) {
+            System.out.println("5");
+
+        } else throw new IOException();
     }
 }
